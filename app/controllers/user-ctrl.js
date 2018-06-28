0857f81fb73ed244ec93c146987d3fc30bc7dc26
Tim Z Liu <995332120@qq.com>
2018-06-28 09:29:30 +0800

    optimize

--- app/controllers/user-ctrl.js
+++ app/controllers/user-ctrl.js
@@ -2,12 +2,8 @@
 let userService = require('../service/user-server');
 
 exports.getUser = async (ctx) => {
-    try {
-        let body = ctx.params;
-        let result = await userService.getUser(body);
-        ctx.body = result;
-    } catch (error) {
-        ctx.body = error.toString();
-    }
+    let body = ctx.params;
+    let result = await userService.getUser(body);
+    ctx.body = result;
 };
 
